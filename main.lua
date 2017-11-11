80f65dc106bf001fe257d3fc13c1a81b13a1ebb9
Zhivko Bogdanov <zhivkobog@github.com>
2017-11-11 12:38:20 +0000

    left/right key bindings.

--- main.lua
+++ main.lua
@@ -1,15 +1,15 @@
 wf = require 'windfield'
 
--- The finished player object
+-- The finished cat object
 cat = {
-  x = 16,
-  y = 16,
+  x = 256,
+  y = 256,
   -- The first set of values are for our rudimentary physics system
   xVelocity = 0, -- current velocity on x, y axes
   yVelocity = 0,
-  acc = 100, -- the acceleration of our player
+  acc = 100, -- the acceleration of our cat
   maxSpeed = 600, -- the top speed
-  friction = 20, -- slow our player down - we could toggle this situationally to create icy or slick platforms
+  friction = 20, -- slow our cat down - we could toggle this situationally to create icy or slick platforms
   gravity = 80, -- we will accelerate towards the bottom
 
   -- These are values applying specifically to jumping
@@ -30,7 +30,18 @@ end
 
 -- Updating
 function love.update(dt)
+	cat.x = cat.x + cat.xVelocity
+  	cat.y = cat.y + cat.yVelocity
 
+  	-- Apply Friction
+  	cat.xVelocity = cat.xVelocity * (1 - math.min(dt * cat.friction, 1))
+  	cat.yVelocity = cat.yVelocity * (1 - math.min(dt * cat.friction, 1))
+
+	if love.keyboard.isDown("left", "a") and cat.xVelocity > -cat.maxSpeed then
+		cat.xVelocity = cat.xVelocity - cat.acc * dt
+	elseif love.keyboard.isDown("right", "d") and cat.xVelocity < cat.maxSpeed then
+		cat.xVelocity = cat.xVelocity + cat.acc * dt
+	end
 end
 
 -- Drawing
