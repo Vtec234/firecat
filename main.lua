2e87c34f251b6387e1dcd4574d8f622e177ccd6e
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2017-11-11 12:39:04 +0000

    physics

--- main.lua
+++ main.lua
@@ -25,15 +25,17 @@ cat = {
 
 -- Loading
 function love.load(arg)
-
+  world = wf.newWorld(0, 0, true)
+  world:newRectangleCollider(200, 300, 20, 20)
 end
 
 -- Updating
 function love.update(dt)
-
+  world:update(dt)
 end
 
 -- Drawing
-function love.draw(dt)
+function love.draw()
+  world:draw()
 	love.graphics.draw(cat.img, cat.x, cat.y)
 end
