a6e455d0e64369b5509123b04fb0f395df344135
Zhivko Bogdanov <zhivkobog@github.com>
2017-11-11 12:25:24 +0000

    physics.

---
+++ conf.lua
@@ -0,0 +1,10 @@
+-- Configuration
+function love.conf(t)
+	t.title = "Firecat" -- The title of the window the game is in (string)
+	t.version = "0.10.2"         -- The LÖVE version this game was made for (string)
+	t.window.width = 1280
+	t.window.height = 720
+
+	-- For Windows debugging
+	t.console = false
+end
--- main.lua
+++ main.lua
@@ -1,3 +1,39 @@
-function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+wf = require 'windfield'
+
+-- The finished player object
+cat = {
+  x = 16,
+  y = 16,
+  -- The first set of values are for our rudimentary physics system
+  xVelocity = 0, -- current velocity on x, y axes
+  yVelocity = 0,
+  acc = 100, -- the acceleration of our player
+  maxSpeed = 600, -- the top speed
+  friction = 20, -- slow our player down - we could toggle this situationally to create icy or slick platforms
+  gravity = 80, -- we will accelerate towards the bottom
+
+  -- These are values applying specifically to jumping
+  isJumping = false, -- are we in the process of jumping?
+  isGrounded = false, -- are we on the ground?
+  hasReachedMax = false, -- is this as high as we can go?
+  jumpAcc = 500, -- how fast do we accelerate towards the top
+  jumpMaxSpeed = 9.5, -- our speed limit while jumping
+
+  -- Here are some incidental storage areas
+  img = love.graphics.newImage('assets/cat.png') -- store the sprite we'll be drawing
+}
+
+-- Loading
+function love.load(arg)
+
+end
+
+-- Updating
+function love.update(dt)
+
+end
+
+-- Drawing
+function love.draw(dt)
+	love.graphics.draw(cat.img, cat.x, cat.y)
 end
