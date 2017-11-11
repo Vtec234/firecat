59f905f39213e49b7826f14e4e913b71e03bfeb4
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2017-11-11 12:12:24 +0000

    circle

--- main.lua
+++ main.lua
@@ -1,3 +1,13 @@
+function love.load()
+  x, y = 500, 20
+end
+
+function love.update(dt)
+  if y > 0 then
+    y = y + 5 * dt
+  end
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+  love.graphics.circle("fill", x, y, 4)
 end
