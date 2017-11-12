5ce524f0788063b76b5761e6ae8559d9b438f8c0
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2017-11-12 13:58:18 +0000

    bars

--- Game.gd
+++ Game.gd
@@ -3,18 +3,26 @@ extends Node
 enum Scene { MENU, SCORE, CREDITS, KITCHEN, LIVING_ROOM }
 var scene = Scene.KITCHEN
 var alarm_on = false
-var burnt_meter = 0
+const BURNAGE_LIMIT = 1000
+const FIRE_DEPT_DELAY = 60
+var burnage_left = BURNAGE_LIMIT
 var time_till_dept_arrival = 0
 # Time in seconds from first fire to fire dept. arrival
-const FIRE_DEPT_DELAY = 60
+
 
 onready var cam = self.get_node("Camera")
 onready var cat = self.get_node("Cat")
+onready var fd_meter_c = cam.get_node("MeterFD").get_node("MeterFDC")
+onready var fd_meter_r = cam.get_node("MeterFD").get_node("MeterFDR")
+onready var br_meter_c = cam.get_node("MeterBR").get_node("MeterBRC")
+onready var br_meter_r = cam.get_node("MeterBR").get_node("MeterBRR")
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	self.set_process(true)
+	var sampl = preload("res://sounds/c23_gamejam.wav")
+	#StreamPlayer(sampl)
 	
 func _process(dt):
 	# Adjust camera
@@ -23,14 +31,24 @@ func _process(dt):
 	
 	# Count fires
 	var fires_started = self.get_tree().get_nodes_in_group("on_fire").size()
-	if fires_started != 0:
+	if fires_started != 0 and alarm_on == false:
 		alarm_on = true
+		cam.get_node("MeterFD").set_hidden(false)
+		cam.get_node("MeterBR").set_hidden(false)
 		time_till_dept_arrival = FIRE_DEPT_DELAY
 		
-	burnt_meter += fires_started * dt * 10
+	burnage_left -= fires_started * dt
+	
+	# Update meters
+	var br_scale = burnage_left/BURNAGE_LIMIT*20
+	br_meter_c.set_scale(Vector2(br_scale, 1))
+	br_meter_r.set_pos(Vector2(12 + br_scale*12, 0))
+	
+	var fd_scale = time_till_dept_arrival/FIRE_DEPT_DELAY*20
+	fd_meter_c.set_scale(Vector2(fd_scale, 1))
+	fd_meter_r.set_pos(Vector2(12 + fd_scale*12, 0))
 	
 	# Check win/lose condition
 	time_till_dept_arrival = max(0, time_till_dept_arrival - dt)
-	#print(time_till_dept_arrival, burnt_meter)
 	if alarm_on == true and time_till_dept_arrival == 0:
-		print("Game over! You got " + String(burnt_meter) + " points!")
+		print("Game over! You got " + String(burnage_left) + " points!")
