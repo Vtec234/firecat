71a1b220b0a7fb423ccaf698c0c1f5042af6bc9b
Tatsuya Haddow <in2erval@outlook.com>
2017-11-12 11:14:38 +0000

    Fire on the cooker baby

--- Interactable.gd
+++ Interactable.gd
@@ -12,14 +12,19 @@ func _ready():
 func interact():
 	for c in self.get_children():
 		if c.get_type() == "RigidBody2D":
-			c.set_pos(self.get_global_pos() + c.get_pos())
 			c.set_mode(0) # MODE_RIGID
 			c.set_hidden(false)
 			
 	if self.has_node("SpriteReplacer"):
 		var rpl = self.get_node("SpriteReplacer")
-		rpl.set_pos(self.get_global_pos())
-		rpl.set_scale(self.get_scale())
 		rpl.set_hidden(false)
+		
+	for c in self.get_children():
+		c.set_pos(c.get_global_pos())
+		c.set_scale(self.get_scale() * c.get_scale())
+		
+	if self.has_node("stovetop"):
+		self.get_node("stovetop").on_fire_nearby(self.get_node("stovetop").get_global_pos())
+	
 	
 	self.remove_and_skip()
